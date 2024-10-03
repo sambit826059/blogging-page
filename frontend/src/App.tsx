f5741ab8a3f660f6319a6da8614bf7743697e1cd
Sambit Mohanty <sambitmohanty1947@gmail.com>
2024-10-03 23:59:36 +0530

    blog and publish routers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
+import { Blogs } from "./pages/Blogs";
+import { Publish } from "./pages/Publish";
 function App() {
 
   return (
@@ -9,8 +11,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
+
           <Route path="/signin" element={<Signin />} />
-          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
     </>
