cf54e8103fcd755b6a77ea764b2671b3c837662b
Sambit Mohanty <sambitmohanty1947@gmail.com>
2024-10-03 23:58:53 +0530

    modifying blog page

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,6 +1,33 @@
 
+import { Appbar } from "../components/Appbar";
+import { Blogpost } from "../components/Blogpost";
+import { Spinner } from "../components/Spinner";
+import { useBlog } from "../hooks";
+import { useParams } from "react-router-dom";
+
 export const Blog = () => {
-  return <div>
-    blog
-  </div>
-}
+  const { id } = useParams();
+  const { loading, blog } = useBlog({
+    id: id || ""
+  });
+
+  if (loading) {
+    return (
+      <div>
+        <div>
+          <Appbar />
+        </div>
+        <div className="flex justify-center items-center min-h-screen">
+          <Spinner />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Blogpost blog={blog} />
+    </div>
+  );
+};
+
